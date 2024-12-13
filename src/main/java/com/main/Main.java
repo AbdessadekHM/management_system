d3c055d0fe371e5c854be9179e3109aee9963703
AbdessadekHm <abdessadekhm@gmail.com>
2024-12-13 14:34:05 +0100

    Fix javafx bug

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -1,8 +1,33 @@
 package com.main;
 
+import java.io.IOException;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Main extends Application{
     public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader  fxmlLoader = new FXMLLoader(Main.class.getResource("/View/form.fxml"));
+        Scene scene;
+        try {
+            scene = new Scene(fxmlLoader.load());
+            stage.setTitle("add to db");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            System.out.println("hello");
+            e.printStackTrace();
+        }
+
     }
 
 
---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module com.main {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires lombok;
+    exports com.main;
+}
