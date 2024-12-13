de965b3fad8e13969d51043027225979c35bbc5b
AbdessadekHm <abdessadekhm@gmail.com>
2024-12-13 10:34:53 +0100

    Finish basic member doa implementation

--- src/main/java/com/main/Dao/MemberDaoImpl.java
+++ src/main/java/com/main/Dao/MemberDaoImpl.java
@@ -1,23 +1,55 @@
 package com.main.Dao;
 
-import java.util.List;
+import java.sql.*;
+import java.util.*;
 
 import com.main.Dao.blueprints.MemberDao;
 import com.main.Model.Incident;
 import com.main.Model.Membre;
 
 public class MemberDaoImpl implements MemberDao {
+    private Connection conn;
 
+    private static MemberDaoImpl instance = new MemberDaoImpl();
     @Override
     public List<Incident> chargetListIncidents() {
-        // TODO Auto-generated method stub
-        return null;
+        String statement = "SELECT * FROM incidents WHERE ";
+
+        try {
+            Statement stmt = conn.prepareStatement(statement);
+            ResultSet rs = stmt.executeQuery(statement);
+            
+            List<Incident> incidents = new ArrayList<>();
+            while(rs.next()){
+                incidents.add(new Incident(rs.getInt("reference"), rs.getString("status"),rs.getString("time"), rs.getInt("membre_id")));
+
+            }
+            return incidents;
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return null;
+        }
     }
 
     @Override
     public void insere(Membre m) {
         // TODO Auto-generated method stub
+        String statement = "INSERT INTO membres(identifiant, nom, prenom, email, phone) VALUES ("+m.getIdentifiant()+","+m.getNom()+","+m.getPrenom()+","+m.getEmail()+","+m.getPhone();
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement(statement);
+            stmt.execute();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         
+
+    }
+
+    public MemberDaoImpl getInstance(){
+        return instance;
     }
      
 }
