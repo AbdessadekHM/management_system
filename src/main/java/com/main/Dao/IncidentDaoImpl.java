71727b99fc21f036e5b1715dc066895581fd764f
AbdessadekHm <abdessadekhm@gmail.com>
2024-12-13 09:33:33 +0100

    Add basic dao implementation

---
+++ src/main/java/com/main/Dao/IncidentDaoImpl.java
@@ -0,0 +1,22 @@
+package com.main.Dao;
+
+import java.util.Set;
+
+import com.main.Dao.blueprints.IncidentDao;
+import com.main.Model.Incident;
+
+public class IncidentDaoImpl implements IncidentDao {
+
+    @Override
+    public void inser(Set<Incident> is) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void inserer(Incident i) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
---
+++ src/main/java/com/main/Dao/MemberDaoImpl.java
@@ -0,0 +1,23 @@
+package com.main.Dao;
+
+import java.util.List;
+
+import com.main.Dao.blueprints.MemberDao;
+import com.main.Model.Incident;
+import com.main.Model.Membre;
+
+public class MemberDaoImpl implements MemberDao {
+
+    @Override
+    public List<Incident> chargetListIncidents() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void insere(Membre m) {
+        // TODO Auto-generated method stub
+        
+    }
+     
+}
