6076296dc7c3b4a87cc22473fe0d2a862f471a11
AbdessadekHm <abdessadekhm@gmail.com>
2024-12-13 10:58:22 +0100

    add view

--- src/main/java/com/main/Main.java
+++ src/main/java/com/main/Main.java
@@ -1,21 +1,9 @@
 package com.main;
 
-import java.sql.Connection;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import com.main.Dao.utils.ConnectionToDB;
 
 public class Main {
     public static void main(String[] args) {
-        LocalDateTime time = LocalDateTime.now();
-        System.out.println(time);
-        DateTimeFormatter formatObj = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String formatedTime = formatObj.format(time);
-
-        System.out.println(formatedTime);
-        System.out.println("Hello world!");
-
-        Connection conn = ConnectionToDB.getInstance().getConnection();
     }
+
+
 }
